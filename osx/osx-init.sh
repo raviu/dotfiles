d025edba01be8c68f4892573fffaca17ae4ac0ab
raviu <ravi.undupitiya@gmail.com>
2024-07-03 20:22:05 +1000

    osx-init.sh: add backticks

--- osx/osx-init.sh
+++ osx/osx-init.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/init-dotfiles/
 cd $HOME/init-dotfiles
 
 echo "Installing homebrew..." 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
 
 echo "Updating bash..."
 brew install bash
